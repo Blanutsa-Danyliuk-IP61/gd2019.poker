ac43c98efd5409de2b9b1271221053e55f88369d
dblanutsa <dmitriy.blanutsa@gmail.com>
2019-10-26 19:40:26 +0300

    first commit

---
+++ server/src/test/java/gd9/poker/poker/PokerApplicationTests.java
@@ -0,0 +1,13 @@
+package gd9.poker.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
